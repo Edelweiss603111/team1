3b5cd324201556488308756df1b51393ed1ada83
Edelweiss603111 <arinavaruhina@gmail.com>
2017-04-14 23:25:02 +0500

    Добавление стилей кнопки и описания

--- views/component/pages/question/questionBanner/questionBanner.js
+++ views/component/pages/question/questionBanner/questionBanner.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 import questionBanner from '../../../../source/img/plug.jpg';
 
 export default class QuestionBanner extends React.Component {
+    static propTypes = {
+        title: React.PropTypes.string,
+    };
+
     render() {
         return (
             <div className='questionBanner'>
                 <img src={questionBanner} alt='questionBanner'/>
-                <span>TitleTitleTitleTitleTitleTitleTitle</span>
-                <input type="submit" className='takeThisTask' value='Участвовать'/>
+                <span>{this.props.title}</span>
+                <input type='submit' className='takeThisTask' value='Участвовать'/>
             </div>
         );
     }
